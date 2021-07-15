b54fac3bd6b4301b78cf56cdf54fa3366f6d5bd5
ptruesdell <parkertruesdell@gmail.com>
2021-07-14 20:27:47 -0600

    Finished setting up db and added user app

---
+++ app/user/tests/test_user_api.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+CREATE_USER_URL = reverse('user':'create')
+
+
+def create_user(**param):
+    return get_user_model().objects.create_user(**params)
+
+
+class PublicUserApiTests(TestCase):
+    """Test the users API (public)"""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_valid_user_success(self):
+        """ Test creating user with valid payload is successful"""
+        payload = {
+            'email' = "test@email.com",
+            'password' = "password123",
+            'name' = 'John Doe'
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
+        user = get_user_model().objects.get(**res.data)
+        self.assertTrue(user.check_password(payload['password']))
+        self.assertNotIn('password', res.data)
